21a7934cca4ac0f70f16ebec3749cb1e0d5fdc2b
shshjmakerspace <shakilash795@gmail.com>
2022-11-08 10:16:23 +0900

    Adding blink section

--- Arduino Unity3D Serial Communication/step2-blink/step2-blink.ino
+++ Arduino Unity3D Serial Communication/step2-blink/step2-blink.ino
@@ -1,17 +1,3 @@
-## Step 1
-Preparing requirements for Arduino
-- Arduino IDE (version 2.0.1 for windows 10)
-- Arduino Uno R3 Board
-- USB cable to connect Arduino to the PC
-
-## Step 2
-Testing a simple blinking code on Arduino
-To do so we can use built-in led of Arduino which is connected to pin #13 in Uno R3. 
-Note: select the correct port and board model from tools menu.
-
-
-```
-// Arduino code
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
@@ -24,8 +10,3 @@ void loop() {
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
   delay(1000);                      // wait for a second
 }
-```
-
-
-
-
