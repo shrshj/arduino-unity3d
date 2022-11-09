469f368820cb3bfa45a04371c7f7d946b179a493
shshjmakerspace <shakilash795@gmail.com>
2022-11-09 17:22:23 +0900

    wifi connection and handling led in Blynk finished

---
+++ ESP32 and Unity3D wifi Connection/control-led-from-phone-blynk/control-led-from-phone-blynk.ino
@@ -0,0 +1,50 @@
+
+// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
+// See the Device Info tab, or Template settings
+#define BLYNK_TEMPLATE_ID           "TMPLxexYg_oz"
+#define BLYNK_DEVICE_NAME           "LED"
+#define BLYNK_AUTH_TOKEN            "q0wAXhdIfF0VrpyhI2huPJRFwKcksGgC"
+
+// Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "PDIWindow";
+char pass[] = "cdtcdtcdt";
+
+// Select your pin with physical button
+int pin = 21;
+int value = 0;
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  pinMode(pin, OUTPUT);
+
+}
+
+
+BLYNK_WRITE(V1)
+{   
+  value = param.asInt(); // Get value as integer
+  if(value){
+    digitalWrite(pin, HIGH);
+  }
+  else{
+    digitalWrite(pin, LOW);
+  }
+
+}
+
+void loop()
+{
+  Blynk.run();
+
+}
--- ESP32 and Unity3D wifi Connection/wifi-conn/wifi-conn.ino
+++ ESP32 and Unity3D wifi Connection/wifi-conn/wifi-conn.ino
@@ -1,16 +1,53 @@
+#include <WiFi.h>;
+#include <ESP32Ping.h>;
 
-// the setup function runs once when you press reset or power the board
-int ledPin = 17;
+bool pingsuccess = false;
+char iptoping[] = "192.168.0.73"; // or url
+
+const char* ssid = "PDIWindow";
+const char* password =  "cdtcdtcdt";
+
 
 void setup() {
-  // initialize digital pin GIOP18 as an output.
-  pinMode(ledPin, OUTPUT);
-}
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  delay(2000);
+  Serial.print("Connecting to WiFi");
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+ 
+  Serial.println("\nConnected to the WiFi network.\n");
 
-// the loop function runs over and over again forever
+
+}
+ 
 void loop() {
-  digitalWrite(ledPin, HIGH); // turn the LED on
-  delay(500);             // wait for 500 milliseconds
-  digitalWrite(ledPin, LOW);  // turn the LED off
-  delay(500);             // wait for 500 milliseconds
+  if((WiFi.status() == WL_CONNECTED)){ //Check the current connection status
+    Serial.println("\n------------------------------------------------------");
+    Serial.println("You can try to ping me. My ip is...");
+    Serial.println(WiFi.localIP());
+    Serial.println("\n");
+
+    Serial.println("I am also trying to ping below address...");
+    Serial.println(iptoping);
+    pingsuccess = Ping.ping(iptoping); // ip or url to ping by me
+    Serial.println("\n");
+
+    if(!pingsuccess){
+      Serial.println("Ping failed");
+    }
+    else{
+      Serial.println("Ping succesful.");
+    }
+    
+    delay(10000);
+
+  }
+  else
+  {
+    Serial.println("Connection lost");
+  }
 }
